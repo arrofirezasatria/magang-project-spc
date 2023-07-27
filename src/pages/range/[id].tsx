d870e3a3c97a3e7d6b4e6ae716d803745870e502
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2023-07-27 16:59:51 +0700

    style: update

---
+++ src/pages/range/[id].tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import type {
+  InferGetStaticPropsType,
+  GetStaticProps,
+  GetStaticPaths,
+} from "next";
+import { Box } from "@mui/material";
+
+type Repo = {
+  id: number;
+};
+
+export default function Page({ repo }) {
+  // console.log(repo);
+  return <Box>{repo.data.id}</Box>;
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const headers = {
+    Authorization:
+      "Bearer 9c54bfb85749cfdc1ea1f98fb2f1a64b7cac4ad7662fda7a099556577a20343b945b20f2b1b68dfab82266337804834c1a1ef342c8a4c5e2886835ba072f49746a825df9e09c46fa214a33fa384134c89d18c0dae1d142c2c441f5876fa4a984012020b22d38a08b5fc2fd60ce80248ebae5c5c2f9511e84c7cae90cfe3a246c",
+  };
+
+  const res = await fetch(
+    "https://strapi-app-tnshv.ondigitalocean.app/api/motifs?fields[0]=id&pagination[pageSize]=9999",
+    {
+      headers: {
+        Authorization: `Bearer 9c54bfb85749cfdc1ea1f98fb2f1a64b7cac4ad7662fda7a099556577a20343b945b20f2b1b68dfab82266337804834c1a1ef342c8a4c5e2886835ba072f49746a825df9e09c46fa214a33fa384134c89d18c0dae1d142c2c441f5876fa4a984012020b22d38a08b5fc2fd60ce80248ebae5c5c2f9511e84c7cae90cfe3a246c`,
+      },
+    }
+  );
+
+  const response = await res.json();
+
+  console.log(response);
+
+  return {
+    paths: [
+      {
+        params: {
+          id: "69",
+        },
+      },
+      {
+        params: {
+          id: "70",
+        },
+      },
+    ],
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async ({ params }: any) => {
+  const headers = {
+    Authorization:
+      "Bearer 9c54bfb85749cfdc1ea1f98fb2f1a64b7cac4ad7662fda7a099556577a20343b945b20f2b1b68dfab82266337804834c1a1ef342c8a4c5e2886835ba072f49746a825df9e09c46fa214a33fa384134c89d18c0dae1d142c2c441f5876fa4a984012020b22d38a08b5fc2fd60ce80248ebae5c5c2f9511e84c7cae90cfe3a246c",
+  };
+
+  const res = await fetch(
+    "https://strapi-app-tnshv.ondigitalocean.app/api/motifs/" + params.id,
+    {
+      headers: {
+        Authorization: `Bearer 9c54bfb85749cfdc1ea1f98fb2f1a64b7cac4ad7662fda7a099556577a20343b945b20f2b1b68dfab82266337804834c1a1ef342c8a4c5e2886835ba072f49746a825df9e09c46fa214a33fa384134c89d18c0dae1d142c2c441f5876fa4a984012020b22d38a08b5fc2fd60ce80248ebae5c5c2f9511e84c7cae90cfe3a246c`,
+      },
+    }
+  );
+  const repo = await res.json();
+  return { props: { repo } };
+};
