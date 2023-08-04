1bd8ddb06bcaf2ad1c8e7a046602419070b2fa76
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2023-08-04 18:28:27 +0700

    style: update

--- src/pages/range/[id].tsx
+++ src/pages/range/[id].tsx
@@ -4,36 +4,38 @@ import type {
   GetStaticProps,
   GetStaticPaths,
 } from "next";
-import { Box, Button, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Grid,
+  Link,
+  List,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { data } from "cypress/types/jquery";
 import { log } from "console";
 import AppsBar from "@components/AppsBar";
 import { useSelector, useDispatch } from "react-redux";
 import { addToCart } from "store/cartSlice";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import FileDownloadOutlinedIcon from "@mui/icons-material/FileDownloadOutlined";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import AltProductRanges from "@components/pages/range/altProductRanges";
+import Image from "next/image";
+import product from "next-seo/lib/jsonld/product";
 
-export default function Page({ data }: any) {
+export default function Page({ data }) {
   console.log(data);
-  console.log(data.attributes.Price);
+  // console.log(product);
+
+  // console.log(data.attributes.Price);
   const dispatch = useDispatch();
 
-  const p = data?.[0]?.attributes;
-
-  return (
-    <Box>
-      <AppsBar />
-      <Box>{data.attributes.Slug}</Box>
-
-      <Typography>Product name : {data.attributes.Name}</Typography>
-      <Typography>Price : {data.attributes.Price}</Typography>
-      <Button
-        onClick={() => {
-          dispatch(addToCart({ ...data?.[0], quantity: 1 }));
-        }}
-      >
-        add to Cart
-      </Button>
-    </Box>
-  );
+  return <></>;
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -81,10 +83,10 @@ export const getStaticProps = async ({ params }: any) => {
   // console.log(params);
   console.log();
   console.log(params.id);
-  const res = await fetch(
+  const responseProduct = await fetch(
     "https://strapi-app-tnshv.ondigitalocean.app/api/products/" +
       params.id +
-      "?populate[motif][populate][0]=products",
+      "?populate[motif][populate][products][populate]=*",
     {
       headers: {
         Authorization: `Bearer 9c54bfb85749cfdc1ea1f98fb2f1a64b7cac4ad7662fda7a099556577a20343b945b20f2b1b68dfab82266337804834c1a1ef342c8a4c5e2886835ba072f49746a825df9e09c46fa214a33fa384134c89d18c0dae1d142c2c441f5876fa4a984012020b22d38a08b5fc2fd60ce80248ebae5c5c2f9511e84c7cae90cfe3a246c`,
@@ -92,10 +94,29 @@ export const getStaticProps = async ({ params }: any) => {
     }
   );
 
-  const repo = await res.json();
+  const responseMotif = await fetch(
+    "https://strapi-app-tnshv.ondigitalocean.app/api/products/" +
+      params.id +
+      "?populate[motif][populate]=*",
+    {
+      headers: {
+        Authorization: `Bearer 9c54bfb85749cfdc1ea1f98fb2f1a64b7cac4ad7662fda7a099556577a20343b945b20f2b1b68dfab82266337804834c1a1ef342c8a4c5e2886835ba072f49746a825df9e09c46fa214a33fa384134c89d18c0dae1d142c2c441f5876fa4a984012020b22d38a08b5fc2fd60ce80248ebae5c5c2f9511e84c7cae90cfe3a246c`,
+      },
+    }
+  );
 
-  // console.log(repo);
+  console.log(responseProduct);
+  console.log(responseMotif);
+
+  const product = await responseProduct.json();
+  const motif = await responseMotif.json();
+
   return {
-    props: repo,
+    props: {
+      data: {
+        product,
+        motif,
+      },
+    },
   };
 };
