275dff6c9a4dddca6d2786f8a5fa78d27472771e
ariskhuzaini <ariskhuzaini@gmail.com>
2023-08-07 13:13:56 +0700

    chore: product highlight

--- src/pages/range/[id].tsx
+++ src/pages/range/[id].tsx
@@ -496,6 +496,52 @@ export default function Page(props: any) {
                 add to Cart
               </Button>
             </Box>
+            <Grid container spacing={6}>
+              <Grid item xs={12} md={6}>
+                <Box sx={{ width: '50%', height: '273px', position: 'relative' }}>
+                  <Image src={props.productOnly.data.attributes?.Image_Tile_Face.data[0].attributes?.formats.medium.url} fill alt='' />
+                </Box>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Box>
+                  <Box sx={{}}>
+                    <Box sx={{
+                      display: 'flex',
+                    }}>
+                      <Typography sx={{ fontSize: '24px', fontWeight: 'bold' }}>{props.productOnly.data.attributes?.Name} - {props.productOnly.data.attributes?.Code}</Typography>
+                    </Box>
+                    <Typography sx={{ fontSize: '24px', fontWeight: 'medium', color: '#999' }}>Rp. {props.productOnly.data.attributes?.Price}</Typography>
+                  </Box>
+                  <Box sx={{
+                    "& .MuiTypography-root": {
+                      fontSize: "16px",
+                      fontWeight: "400",
+                    },
+                  }}>
+                    <Box sx={{ display: 'flex', borderTop: '1px solid #999', width: '100%' }}>
+                      <Typography sx={{ flexBasis: '50%' }}>Code:</Typography>
+                      <Typography sx={{ flexBasis: '50%' }}>{props.productOnly.data.attributes?.Code}</Typography>
+                    </Box>
+                    <Box sx={{ display: 'flex', borderTop: '1px solid #999', width: '100%' }}>
+                      <Typography sx={{ flexBasis: '50%' }}>Size:</Typography>
+                      <Typography sx={{ flexBasis: '50%' }}>{props.productOnly.data.attributes?.tile_dimension.data.attributes?.Dimension}</Typography>
+                    </Box>
+                    <Box sx={{ display: 'flex', borderTop: '1px solid #999' }}>
+                      <Typography sx={{ flexBasis: '50%' }}>Finish:</Typography>
+                      <Typography sx={{ flexBasis: '50%' }}>{props.productOnly.data.attributes?.surface_finish.data?.attributes?.Name}</Typography>
+                    </Box>
+                    <Box sx={{ display: 'flex', borderTop: '1px solid #999' }}>
+                      <Typography sx={{ flexBasis: '50%' }}>Color:</Typography>
+                      <Typography sx={{ flexBasis: '50%' }}>{props.productOnly.data.attributes?.Motif_Color}</Typography>
+                    </Box>
+                    <Box sx={{ display: 'flex', borderTop: '1px solid #999' }}>
+                      <Typography sx={{ flexBasis: '50%' }}>Face:</Typography>
+                      <Typography sx={{ flexBasis: '50%' }}>{props.productOnly.data.attributes?.N_Face}</Typography>
+                    </Box>
+                  </Box>
+                </Box>
+              </Grid>
+            </Grid>
           </Box>
         </Box>
         <Box
@@ -753,8 +799,8 @@ export const getStaticProps = async ({ params }: any) => {
   console.log(params.id);
   const responseProduct = await fetch(
     "https://strapi-app-tnshv.ondigitalocean.app/api/products/" +
-      params.id +
-      "?populate[motif][populate][products][populate]=*",
+    params.id +
+    "?populate[motif][populate][products][populate]=*",
     {
       headers: {
         Authorization: `Bearer 9c54bfb85749cfdc1ea1f98fb2f1a64b7cac4ad7662fda7a099556577a20343b945b20f2b1b68dfab82266337804834c1a1ef342c8a4c5e2886835ba072f49746a825df9e09c46fa214a33fa384134c89d18c0dae1d142c2c441f5876fa4a984012020b22d38a08b5fc2fd60ce80248ebae5c5c2f9511e84c7cae90cfe3a246c`,
@@ -764,8 +810,8 @@ export const getStaticProps = async ({ params }: any) => {
 
   const responseMotif = await fetch(
     "https://strapi-app-tnshv.ondigitalocean.app/api/products/" +
-      params.id +
-      "?populate[motif][populate]=*",
+    params.id +
+    "?populate[motif][populate]=*",
     {
       headers: {
         Authorization: `Bearer 9c54bfb85749cfdc1ea1f98fb2f1a64b7cac4ad7662fda7a099556577a20343b945b20f2b1b68dfab82266337804834c1a1ef342c8a4c5e2886835ba072f49746a825df9e09c46fa214a33fa384134c89d18c0dae1d142c2c441f5876fa4a984012020b22d38a08b5fc2fd60ce80248ebae5c5c2f9511e84c7cae90cfe3a246c`,
@@ -775,8 +821,8 @@ export const getStaticProps = async ({ params }: any) => {
 
   const responseAmbience = await fetch(
     "https://strapi-app-tnshv.ondigitalocean.app/api/products/" +
-      params.id +
-      "?populate=*",
+    params.id +
+    "?populate=*",
     {
       headers: {
         Authorization: `Bearer 9c54bfb85749cfdc1ea1f98fb2f1a64b7cac4ad7662fda7a099556577a20343b945b20f2b1b68dfab82266337804834c1a1ef342c8a4c5e2886835ba072f49746a825df9e09c46fa214a33fa384134c89d18c0dae1d142c2c441f5876fa4a984012020b22d38a08b5fc2fd60ce80248ebae5c5c2f9511e84c7cae90cfe3a246c`,
