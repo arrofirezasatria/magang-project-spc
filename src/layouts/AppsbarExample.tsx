f24a989b5883cc3dac302f1676206cc3f024f135
ariskhuzaini <ariskhuzaini@gmail.com>
2023-08-21 15:15:51 +0700

    style: drawer update

--- src/layouts/AppsbarExample.tsx
+++ src/layouts/AppsbarExample.tsx
@@ -13,6 +13,8 @@ import {
   TextField,
 } from "@mui/material";
 import React, { useState } from "react";
+import { NumericFormat } from "react-number-format";
+import Image from "next/image";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import AddIcon from "@mui/icons-material/Add";
@@ -101,19 +103,6 @@ export default function AppsbarExample() {
             </Box>
             {true ? (
               <>
-                {cart.map((item, index) => {
-                  return (
-                    <Stack key={index}>
-                      <Typography>{item.id}</Typography>
-                      <Typography>{item.code}</Typography>
-                      <Typography>{item.name}</Typography>
-                      <Typography>{item.dimension}</Typography>
-                      <Typography>{item.quantity}</Typography>
-                      <Typography>{item.pricePerBox}</Typography>
-                      <Typography>{item.priceTotal}</Typography>
-                    </Stack>
-                  );
-                })}
                 <List
                   sx={{
                     p: "1rem",
@@ -121,114 +110,138 @@ export default function AppsbarExample() {
                     flexGrow: "1",
                   }}
                 >
-                  <ListItem
-                    sx={{ borderBottom: "1px solid #ededed", py: "1rem" }}
-                  >
-                    <Box
-                      sx={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        width: "100%",
-                      }}
-                    >
-                      <Box sx={{ display: "flex" }}>
-                        <Box>
-                          <IconButton
-                            sx={{
-                              position: "absolute",
-                              width: "15px",
-                              height: "15px",
-                              bgcolor: "#999",
-                              ml: "50px",
-                              mt: "-0.3rem",
-                              "&:hover": {
-                                bgcolor: "#999",
-                              },
-                            }}
-                          >
-                            <CloseIcon
-                              sx={{ fontSize: "15px", color: "#fff" }}
-                            />
-                          </IconButton>
-                          <Box
-                            sx={{
-                              bgcolor: "#ededed",
-                              width: "65px",
-                              height: "65px",
-                              borderRadius: "0.375rem",
-                            }}
-                          >
-                            {/* <Image src={ } alt="" fill /> */}
+                  {cart.map((item, index) => {
+                    return (
+                      // <Stack key={index}>
+                      //   <Typography>{item.id}</Typography>
+                      //   <Typography>{item.code}</Typography>
+                      //   <Typography>{item.name}</Typography> SUDAH
+                      //   <Typography>{item.dimension}</Typography> SUDAH
+                      //   <Typography>{item.quantity}</Typography>
+                      //   <Typography>{item.pricePerBox}</Typography> SUDAH
+                      //   <Typography>{item.priceTotal}</Typography>
+                      //   <Typography>{item.imageSrc}</Typography>
+                      // </Stack>
+                      <ListItem
+                        sx={{ borderBottom: "1px solid #ededed", py: "1rem" }}
+                      >
+                        <Box
+                          sx={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            width: "100%",
+                          }}
+                        >
+                          <Box sx={{ display: "flex" }}>
+                            <Box>
+                              <IconButton
+                                sx={{
+                                  position: "absolute",
+                                  zIndex: 999,
+                                  width: "15px",
+                                  height: "15px",
+                                  bgcolor: "#DC362E",
+                                  ml: "55px",
+                                  mt: "-0.3rem",
+                                  textAlign: 'center',
+                                  "&:hover": {
+                                    bgcolor: "#ff3333",
+                                  },
+                                }}
+                              >
+                                <CloseIcon
+                                  sx={{ fontSize: "15px", color: "#fff" }}
+                                />
+                              </IconButton>
+                              <Box
+                                sx={{
+                                  width: "65px",
+                                  height: "65px",
+                                  border: '1px solid #ededed',
+                                  borderRadius: "0.375rem",
+                                  position: 'relative',
+                                }}
+                              >
+                                <Image src={item.imageSrc} fill alt="" />
+                              </Box>
+                            </Box>
+                            <Box
+                              sx={{
+                                ml: "16px",
+                                overflowWrap: "break-word",
+                                flexWrap: "wrap",
+                                maxWidth: "130px",
+                              }}
+                            >
+                              <Typography
+                                sx={{ fontSize: "16px", fontWeight: "medium" }}
+                              >
+                                {item.name}
+                              </Typography>
+                              <Typography
+                                sx={{ fontSize: "14px", color: "#737373" }}
+                              >
+                                {item.dimension}
+                              </Typography>
+                            </Box>
+                          </Box>
+                          <Box>
+                            <Typography sx={{ fontSize: "14px" }}>
+                              <NumericFormat
+                                // value={item.pricePerBox * item.quantity}
+                                value={item.priceTotal}
+                                decimalScale={3}
+                                displayType={"text"}
+                                thousandSeparator={true}
+                                prefix={"Rp. "}
+                              />
+                            </Typography>
+                            <Box
+                              display="flex"
+                              alignItems="center"
+                              sx={{
+                                border: "1px solid #999",
+                                borderRadius: "9999px",
+                                justifyContent: "space-between",
+                              }}
+                            >
+                              <IconButton
+                              // onClick={() => handleDecrement(index)}
+                              >
+                                <RemoveIcon sx={{ fontSize: "15px" }} />
+                              </IconButton>
+                              <TextField
+                                sx={{
+                                  width: "24px",
+                                  "& .MuiInputBase-input": {
+                                    textAlign: "center",
+                                    fontSize: "14px",
+                                  },
+                                  "& input[type=number]::-webkit-inner-spin-button, & input[type=number]::-webkit-outer-spin-button":
+                                  {
+                                    appearance: "none",
+                                  },
+                                }}
+                                type="number"
+                                value={item.quantity}
+                                // onChange={(event) => handleChange(event, index)}
+                                variant="standard"
+                                InputProps={{
+                                  disableUnderline: true,
+                                }}
+                                size="small"
+                              />
+                              <IconButton
+                              // onClick={() => handleIncrement(index)}
+                              >
+                                <AddIcon sx={{ fontSize: "15px" }} />
+                              </IconButton>
+                            </Box>
                           </Box>
                         </Box>
-                        <Box
-                          sx={{
-                            ml: "16px",
-                            overflowWrap: "break-word",
-                            flexWrap: "wrap",
-                            maxWidth: "130px",
-                          }}
-                        >
-                          <Typography
-                            sx={{ fontSize: "16px", fontWeight: "medium" }}
-                          >
-                            Le Product Ranges
-                          </Typography>
-                          <Typography
-                            sx={{ fontSize: "14px", color: "#737373" }}
-                          >
-                            Black / 120x60cm
-                          </Typography>
-                        </Box>
-                      </Box>
-                      <Box>
-                        <Typography sx={{ fontSize: "14px" }}>
-                          Rp. 123.123.123
-                        </Typography>
-                        <Box
-                          display="flex"
-                          alignItems="center"
-                          sx={{
-                            border: "1px solid #999",
-                            borderRadius: "9999px",
-                            justifyContent: "space-between",
-                          }}
-                        >
-                          <IconButton
-                          // onClick={() => handleDecrement(index)}
-                          >
-                            <RemoveIcon sx={{ fontSize: "15px" }} />
-                          </IconButton>
-                          <TextField
-                            sx={{
-                              width: "24px",
-                              "& .MuiInputBase-input": {
-                                textAlign: "center",
-                                fontSize: "14px",
-                              },
-                              "& input[type=number]::-webkit-inner-spin-button, & input[type=number]::-webkit-outer-spin-button":
-                                {
-                                  appearance: "none",
-                                },
-                            }}
-                            type="number"
-                            value={0}
-                            // onChange={(event) => handleChange(event, index)}
-                            variant="standard"
-                            InputProps={{
-                              disableUnderline: true,
-                            }}
-                            size="small"
-                          />
-                          <IconButton
-                          // onClick={() => handleIncrement(index)}
-                          >
-                            <AddIcon sx={{ fontSize: "15px" }} />
-                          </IconButton>
-                        </Box>
-                      </Box>
-                    </Box>
-                  </ListItem>
+                      </ListItem>
+                    );
+                  })}
                 </List>
                 <Box sx={{ p: "1.5rem" }}>
                   <Box
