0f86eecd3e21a95be7a21b27b2d0af3603940842
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2023-08-26 11:29:07 +0700

    style: update

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,11 +1,23 @@
-import { Box, Container, Grid, Stack, Typography, Button, FormControl, InputLabel, MenuItem, Select, Link } from "@mui/material";
+import {
+  Box,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Link,
+} from "@mui/material";
 // import { GetStaticProps } from "next";
 import React from "react";
 import axios from "axios";
 import { data } from "cypress/types/jquery";
 import useSWR from "swr";
 import Image from "next/image";
-import FeaturedProducts from "@components/pages/range/featuredProducts";
+import FeaturedProducts from "@components/pages/range/FeaturedProducts";
 import HeroProducts from "@components/pages/range/HeroProducts";
 import Description from "@components/pages/range/DescriptionProducts";
 import FilterListIcon from "@mui/icons-material/FilterList";
@@ -22,16 +34,25 @@ const headers = {
     "Bearer 9c54bfb85749cfdc1ea1f98fb2f1a64b7cac4ad7662fda7a099556577a20343b945b20f2b1b68dfab82266337804834c1a1ef342c8a4c5e2886835ba072f49746a825df9e09c46fa214a33fa384134c89d18c0dae1d142c2c441f5876fa4a984012020b22d38a08b5fc2fd60ce80248ebae5c5c2f9511e84c7cae90cfe3a246c",
 };
 
-const fetcher2 = (url: RequestInfo | URL) => fetch(url, { headers }).then((res) => res.json());
+const fetcher2 = (url: RequestInfo | URL) =>
+  fetch(url, { headers }).then((res) => res.json());
 
 export default function Index() {
-  const { data, error, isLoading, isValidating } = useSWR(`https://strapi-app-tnshv.ondigitalocean.app/api/motifs?populate=*`, fetcher2);
+  const { data, error, isLoading, isValidating } = useSWR(
+    `https://strapi-app-tnshv.ondigitalocean.app/api/motifs?populate=*`,
+    fetcher2
+  );
 
   const [open, setOpen] = useState(false);
   const DropdownFilter = [
     {
       nama: "-Collections-",
-      Subitem: ["Intro colection", "Select Collection", "Absolute Collection", "Minton Hollins Collection"],
+      Subitem: [
+        "Intro colection",
+        "Select Collection",
+        "Absolute Collection",
+        "Minton Hollins Collection",
+      ],
     },
     {
       nama: "-sizes-",
@@ -68,19 +89,64 @@ export default function Index() {
     },
     {
       nama: "-Styles-",
-      Subitem: ["Stone", "concrete", "Marble", "Wood", "Colours", "White", "Structure", "patern", "Shape", "Speckle", "Mosaic"],
+      Subitem: [
+        "Stone",
+        "concrete",
+        "Marble",
+        "Wood",
+        "Colours",
+        "White",
+        "Structure",
+        "patern",
+        "Shape",
+        "Speckle",
+        "Mosaic",
+      ],
     },
     {
       nama: "-Materials-",
-      Subitem: ["Glazed Ceramic", "Natural Stone & Glass", "Natural Stone", "Glass", "Ceramic", "Un-Glazed Porcelain", "Glazed Vitrified", "Porcelain", "Glazed Porcelain"],
+      Subitem: [
+        "Glazed Ceramic",
+        "Natural Stone & Glass",
+        "Natural Stone",
+        "Glass",
+        "Ceramic",
+        "Un-Glazed Porcelain",
+        "Glazed Vitrified",
+        "Porcelain",
+        "Glazed Porcelain",
+      ],
     },
     {
       nama: "-Ranges-",
-      Subitem: ["1901", "Abstract", "Allure", "Arctic white", "Arlo", "Artisan", "Ashlar", "Atrium", "Baseline Wall", "bellagio", "Bergen", "Bevel", "Bevel Brick", "Bianco", "Blake"],
+      Subitem: [
+        "1901",
+        "Abstract",
+        "Allure",
+        "Arctic white",
+        "Arlo",
+        "Artisan",
+        "Ashlar",
+        "Atrium",
+        "Baseline Wall",
+        "bellagio",
+        "Bergen",
+        "Bevel",
+        "Bevel Brick",
+        "Bianco",
+        "Blake",
+      ],
     },
     {
       nama: "-Suitabillity-",
-      Subitem: ["Wall", "Floor", "Border", "External Wall", "External Floor", "Wet Room"],
+      Subitem: [
+        "Wall",
+        "Floor",
+        "Border",
+        "External Wall",
+        "External Floor",
+        "Wet Room",
+      ],
     },
     {
       nama: "-Colours-",
