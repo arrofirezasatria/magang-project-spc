686cc225779426c1334a43f4e1400df51da3d718
wildanaw18 <wildanaw411@gmail.com>
2023-08-30 10:33:32 +0700

    style: featured,index

--- src/pages/range/index.tsx
+++ src/pages/range/index.tsx
@@ -10,7 +10,9 @@ import { useState } from "react";
 import AddressProduct from "@components/pages/range/AddressProduct";
 import DescriptionProducts from "@components/pages/range/DescriptionProducts";
 import ProductRange from "@components/pages/range/ProductRange";
-// import FeaturedProducts from "@components/pages/range/featuredProducts";
+import FeaturedProducts from "@components/pages/range/FeaturedProduct";
+import ProductLayout from "@layouts/ProductLayout";
+
 
 export default function Index(props: any) {
   return (
@@ -96,9 +98,17 @@ export default function Index(props: any) {
       <Container>
         {/* <AddressProduct address={idRouter} /> */}
         <DescriptionProducts />
-        {/* <FeaturedProducts props={props} /> */}
-        <ProductRange props={props} />
       </Container>
+      <ProductLayout backgroundColor={'#f5f5f5'}>
+        <FeaturedProducts
+          alt1={props.alternative1}
+          alt2={props.alternative2}
+          alt3={props.alternative3}/>
+      </ProductLayout>
+      <ProductLayout>
+        
+        <ProductRange props={props} />
+      </ProductLayout>
     </>
   );
 }
@@ -112,8 +122,54 @@ export const getStaticProps = async () => {
 
   const response = await res.json();
 
+
+  const responseAlt1 = await fetch(
+    "https://strapi-app-tnshv.ondigitalocean.app/api/products/" +
+    153 +
+    "?populate=deep,10",
+    {
+      headers: {
+        Authorization: `Bearer 9c54bfb85749cfdc1ea1f98fb2f1a64b7cac4ad7662fda7a099556577a20343b945b20f2b1b68dfab82266337804834c1a1ef342c8a4c5e2886835ba072f49746a825df9e09c46fa214a33fa384134c89d18c0dae1d142c2c441f5876fa4a984012020b22d38a08b5fc2fd60ce80248ebae5c5c2f9511e84c7cae90cfe3a246c`,
+      },
+    }
+  );
+
+  const responseAlternative1 = await responseAlt1.json();
+
+  const responseAlt2 = await fetch(
+    "https://strapi-app-tnshv.ondigitalocean.app/api/products/" +
+    159 +
+    "?populate=deep,10",
+    {
+      headers: {
+        Authorization: `Bearer 9c54bfb85749cfdc1ea1f98fb2f1a64b7cac4ad7662fda7a099556577a20343b945b20f2b1b68dfab82266337804834c1a1ef342c8a4c5e2886835ba072f49746a825df9e09c46fa214a33fa384134c89d18c0dae1d142c2c441f5876fa4a984012020b22d38a08b5fc2fd60ce80248ebae5c5c2f9511e84c7cae90cfe3a246c`,
+      },
+    }
+  );
+
+  const responseAlternative2 = await responseAlt2.json();
+
+  const responseAlt3 = await fetch(
+    "https://strapi-app-tnshv.ondigitalocean.app/api/products/" +
+    156 +
+    "?populate=deep,10",
+    {
+      headers: {
+        Authorization: `Bearer 9c54bfb85749cfdc1ea1f98fb2f1a64b7cac4ad7662fda7a099556577a20343b945b20f2b1b68dfab82266337804834c1a1ef342c8a4c5e2886835ba072f49746a825df9e09c46fa214a33fa384134c89d18c0dae1d142c2c441f5876fa4a984012020b22d38a08b5fc2fd60ce80248ebae5c5c2f9511e84c7cae90cfe3a246c`,
+      },
+    }
+  );
+
+  const responseAlternative3 = await responseAlt3.json();
+
+
   console.log(response);
   return {
-    props: { response: response },
+    props: {
+      alternative1: responseAlternative1,
+      alternative2: responseAlternative2,
+      alternative3: responseAlternative3,
+      response: response
+    },
   };
 };
